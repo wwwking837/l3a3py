330a2ba6617df80442d2a830fdced9e718909e0f
wwwking837 <muhammadzaroonadnan@gmail.com>
2025-02-11 18:45:31 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+num = int(input("Enter number to check"))
+
+if num>50:
+    print("Number is greater than 50")
+    if num%2==0:
+        print("and it is even too")
+    else:
+        print("and It is odd")
+else:
+    print("Number is less than 50")
+    
+
+    
